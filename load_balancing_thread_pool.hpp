0b3f7eb4a61bf41782eb0507551e8fc7f1205f8d
Alex Zywicki <alexander.zywicki@gmail.com>
2016-11-10 18:38:27 -0500

    removing overhead in worker

--- load_balancing_thread_pool.hpp
+++ load_balancing_thread_pool.hpp
@@ -13,47 +13,24 @@
 #include <vector>
 #include <numeric>
 #include <cmath>
-#include <iostream>
 
-/*
- design:
- 1 queue per thread, all single producer single consumer
- N worker threads
- 1 conductor in calling thread that will manage who gets handed work
-
- push(function,data){
- bool pushed = false;
- for each thread{
- if N jobs pending < average load as integer
- give work to that thread
- pushed = true;
- }
- if(not pushed){
- figure out who has least work pending
- give to that thread
- }
- }
- }
- }
-
- */
 
 template< template<typename...> class queue_t, template<typename...> class allocator_t = std::allocator>
-class basic_load_balancing_thread_pool{
+class basic_load_balancing_thread_pool
+{
 private:
     using work_signiture = void();
     using work_type = std::function<work_signiture>;
     struct thread_data
     {
-        thread_data():_lock(new std::mutex()),_load(new std::atomic<std::size_t>(0)),_quit(new std::atomic_bool(false)){
-
-        }
+        thread_data():_lock(new std::mutex()),_load(new std::atomic<std::size_t>(0)),_quit(new std::atomic_bool(false)){}
         thread_data(thread_data&& other):_thread(std::move(other._thread)),
                                          _lock(std::move(other._lock)),
                                          _load(std::move(other._load)),
                                          _quit(std::move(other._quit)),
                                          _work(std::move(other._work)){}
-        thread_data& operator=(thread_data&& other){
+        thread_data& operator=(thread_data&& other)
+        {
             if (this != &other)
             {
                 _thread = std::move(other._thread);
@@ -73,17 +50,18 @@ private:
 
 public:
 
-    basic_load_balancing_thread_pool(std::size_t N = std::thread::hardware_concurrency()) :_running(true),_forced_idle(false)
+    basic_load_balancing_thread_pool(std::size_t N = std::thread::hardware_concurrency()) :_thread_data(N)
     {
-        _thread_data.resize(N);
-        for(std::size_t i = 0; i < N ; ++i)
-        {
-            _thread_data[i]._thread = std::thread(init_thread(i));
+        for(auto&& th: _thread_data){
+            th._thread = std::thread(init_thread(th));
         }
     }
     ~basic_load_balancing_thread_pool()
     {
-        _running = false;
+        for (auto&& w: _thread_data)
+        {
+            *w._quit = true;
+        }
         _cv.notify_all();
         for(auto&& w : _thread_data)
         {
@@ -102,27 +80,26 @@ public:
     {
         long old = _thread_data.size();
         long diff = N - old;
+        diff = std::abs((double)diff);
         if(N > old)
         {
-            for(std::size_t i =0; i < std::abs(diff); ++i)
+            for(std::size_t i =0; i < diff; ++i)
             {
                 _thread_data.push_back(thread_data{});
-                _thread_data.back()._thread=std::thread(init_thread(old-1+i));
+                _thread_data.back()._thread=std::thread(init_thread(_thread_data.back()));
             }
         }
         else if(old > N)
         {
-
-            for(std::size_t i=0; i < std::abs(diff); ++i)
+            for(std::size_t i=0; i <diff; ++i)
             {
                 *_thread_data.back()._quit = true;
-                if(_thread_data.back()._thread.joinable()){
+                if(_thread_data.back()._thread.joinable())
+                {
                     _thread_data.back()._thread.join();
-                }   
+                }
             }
-
         }
-
     }
 
     basic_load_balancing_thread_pool(basic_load_balancing_thread_pool&)=delete;
@@ -151,13 +128,11 @@ private:
     {
         std::size_t avg_load = average_load();
         bool done=false;
-        for(std::size_t i =0; i <_thread_data.size();++i)
-        {
-            if(*_thread_data[i]._load < avg_load)
-            {
-                std::unique_lock<std::mutex> lk{*_thread_data[i]._lock};
-                _thread_data[i]._work.push_back(std::forward<T&&>(value));
-                ++(*_thread_data[i]._load);
+        for(auto&& data: _thread_data){
+            if(*data._load < avg_load){
+                std::unique_lock<std::mutex> lk{*data._lock};
+                data._work.push_back(std::forward<T&&>(value));
+                ++(*data._load);
                 done = true;
                 break;
             }
@@ -165,21 +140,19 @@ private:
         //this branch will default to 0 if the system is empty
         if (!done)
         {
-            std::size_t lowest_index=0;
-            std::size_t lowest = *_thread_data[0]._load;
-            for(std::size_t i = lowest_index;i<_thread_data.size();++i)
-            {
-                if(*_thread_data[i]._load < lowest)
+            typename std::vector<thread_data>::iterator lowest_index = _thread_data.begin();
+            std::size_t lowest = *lowest_index->_load;
+            for(typename std::vector<thread_data>::iterator data = _thread_data.begin(); data != _thread_data.end(); ++data){
+                if (*data->_load < lowest)
                 {
-                    lowest_index = i;
-                    lowest = _thread_data[i]._work.size();
+                    lowest_index = data;
+                    lowest = data->_work.size();
+                    /* code */
                 }
             }
-            {
-                std::unique_lock<std::mutex> lk{*_thread_data[lowest_index]._lock};
-                _thread_data[lowest_index]._work.push_back(std::forward<T&&>(value));
-                ++(*_thread_data[lowest_index]._load);
-            }
+            std::unique_lock<std::mutex> lk{*lowest_index->_lock};
+            lowest_index->_work.push_back(std::forward<T&&>(value));
+            ++(*lowest_index->_load);
         }
     }
 
@@ -193,30 +166,30 @@ private:
             }
             avg/= _thread_data.size();
             return std::ceil(avg);
-            //average load rounded up
         }
         else
         {
             return {};
         }
     }
-    work_type init_thread(std::size_t index)
+    work_type init_thread(thread_data& _data)
     {
         //where index is the index used to match up the threads to the queues and loads
-        return [this,index](){
-            queue_t<work_type>& q = _thread_data[index]._work;
-            std::atomic<std::size_t>& load = *_thread_data[index]._load;
-            std::mutex& lock = *_thread_data[index]._lock;
-            std::atomic_bool& quit = *_thread_data[index]._quit;
-            while (_running)
+        return [&](){
+            thread_data& data = const_cast<thread_data&>(_data);
+            queue_t<work_type>& q = data._work;
+            std::atomic<std::size_t>& load = *data._load;
+            std::mutex& lock = *data._lock;
+            std::atomic_bool& quit = *data._quit;
+            while (!quit)
             {
                 std::unique_lock<std::mutex> lk{_sleep};
                 _cv.wait(lk,[&](){
-                    return load || !_running || _forced_idle;
+                    return load || quit;
                 });
-                if (!_forced_idle)
+                if (!quit)
                 {
-                    while (load && !_forced_idle)
+                    while (load)
                     {
                         work_type task;
                         {
@@ -227,17 +200,11 @@ private:
                         task();
                         --load;
                     }
-                    if (!_running || quit)
-                    {
-                        break;
-                    }
                 }
             }
         };
     }
     std::vector<thread_data> _thread_data;
-    std::atomic_bool _running;
-    std::atomic_bool _forced_idle;
     std::condition_variable _cv;
     std::mutex _sleep;
 };
